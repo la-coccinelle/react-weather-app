dd60d7a471543778e8b9c1821a42dfd5d661b70d
monikagrz <55914238+monikagrz@users.noreply.github.com>
2022-02-06 18:26:18 +0900

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Weather</div>;
+  return (
+    <div className="App">
+      <h1>Hello Weather</h1>
+      <button className="btn btn-success">Click</button>
+      <footer>
+        Created by Monika Grzegorczyk and{" "}
+        <a
+          href="https://github.com/la-coccinelle/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
 
 export default App;
