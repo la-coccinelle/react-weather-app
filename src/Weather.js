1cd33d07cbd5dd7b20d1ec6bc24f3c894eed7091
monikagrz <55914238+monikagrz@users.noreply.github.com>
2022-02-07 14:53:57 +0900

    Added CSS styling.

--- src/Weather.js
+++ src/Weather.js
@@ -11,27 +11,34 @@ export default function Weather() {
               type="search"
               placeholder="Type a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Warsaw</h1>
-      <div className="row">
+      <ul>
+        <li>Wednesday 7:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
         <div className="col-6">
-          <ul>
-            <li>Wednesday 7:00</li>
-            <li>Mostly Cloudy</li>
-            <li>
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="Partly cloudy"
-              />
-              6°C
-            </li>
-          </ul>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Partly cloudy"
+              className="float-left"
+            />
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
